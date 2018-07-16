0e26f08d2b6e6db6f21c0cf887db87893bf09f2f
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-16 13:58:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n) {
   document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
   for (let i=0; i<lis.length; i++) {
-    lis[i].innerHTML = (lis[i]+n);
+    lis[i].innerHTML = (parseInt(lis[i])+n);
   }
 }
 
